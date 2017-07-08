8d1449a10ae06fa789394e3d3e0beb78858a55ba
Vitalii <kukhar.vitalii@gmail.com>
2017-07-08 20:06:06 +0300

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,4 +1,4 @@
-<!--<template>
+<template>
   <div class="col-sm-4 col-sm-offset-4">
     <h2>Log In</h2>
     <p>Log in to your account to get some great quotes.</p>
@@ -48,44 +48,4 @@ export default {
   }
   
 }
-</script>-->
-
-  <template>
-    <div class="col-sm-4 col-sm-offset-4">
-      <h2>Log In</h2>
-      <p>Log In with Auth0's Lock Widget.</p>
-      <button class="btn btn-primary" @click="login()">Log In</button>
-    </div>
-  </template>
-
-  <script>
-  // Import the Lock instance
-  import {lock} from '../index'
-
-  export default {
-
-    methods: {
-
-      login() {
-
-        // Show the Lock Widget and save the user's JWT on a successful login
-        lock.show((err, profile, id_token) => {
-
-          localStorage.setItem('profile', JSON.stringify(profile))
-          localStorage.setItem('id_token', id_token)
-
-        })
-      },
-
-      logout() {
-
-        // Remove the profile and token from localStorage
-        localStorage.removeItem('profile')
-        localStorage.removeItem('id_token')
-
-      }
-    }
-
-  }
-
-  </script>
+</script>
